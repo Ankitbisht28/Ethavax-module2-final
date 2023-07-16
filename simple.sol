e4090409b5dfd9d47a2c093a179022d83d2a0b37
Ankitbisht28 <127379059+Ankitbisht28@users.noreply.github.com>
2023-07-16 17:50:15 +0530

    Update simple.sol

--- simple.sol
+++ simple.sol
@@ -3,7 +3,13 @@
 pragma solidity >=0.8.7;
 
 contract Simple {
-    
-    string public myCity = "AnkitBisht";
+    string public myCity;
 
+    function getCity() public view returns (string memory) {
+        return myCity;
+    }
+
+    function setCity(string memory newCity) public {
+        myCity = newCity;
+    }
 }
